4fe920924094c6902655ff5dc42b64126e6bf285
samuel hudec <samuel.hudec@gmail.com>
2023-09-01 10:45:03 +0200

    - add test image

--- ml-detection/detect_objects.py
+++ ml-detection/detect_objects.py
@@ -1,6 +1,29 @@
+import torch
 from PIL import Image
 import requests
 from transformers import AutoFeatureExtractor
 from transformers import YolosForObjectDetection
-import torch
 import matplotlib.pyplot as plt
+import argparse
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--image-path",
+        help="path to image in local or url",
+        required=True,
+        default="images/test_image.jpeg"
+    )
+    return parser.parse_args()
+
+
+def main(args: argparse.Namespace) -> None:
+
+
+
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
