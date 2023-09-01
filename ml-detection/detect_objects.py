18c59b429dd5275f22d969ddcb4130d5ab40dcac
samuel hudec <samuel.hudec@gmail.com>
2023-09-01 09:39:43 +0200

    - initial setup

---
+++ ml-detection/detect_objects.py
@@ -0,0 +1,6 @@
+from PIL import Image
+import requests
+from transformers import AutoFeatureExtractor
+from transformers import YolosForObjectDetection
+import torch
+import matplotlib.pyplot as plt
